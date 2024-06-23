8be676e0e9ff531c401a22968c897070bc28d100
orekyuu <orekyuu@gmail.com>
2024-06-23 21:11:19 +0900

    Upgrade to JDK11

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,10 @@ subprojects {
     }
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
         toolchain {
-            languageVersion = JavaLanguageVersion.of(8)
+            languageVersion = JavaLanguageVersion.of(11)
         }
         withSourcesJar()
         withJavadocJar()
