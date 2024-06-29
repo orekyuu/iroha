9fe03ecb8c77d8e8303f3bad00353c0c92d69d63
orekyuu <orekyuu@gmail.com>
2024-06-30 01:08:19 +0900

    Refactor test

--- integration-test/src/main/java/net/orekyuu/iroha/integration/doma/DomaIntegrationTest.java
+++ integration-test/src/main/java/net/orekyuu/iroha/integration/doma/DomaIntegrationTest.java
@@ -1,12 +1,11 @@
 package net.orekyuu.iroha.integration.doma;
 
-import static org.assertj.db.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
-import net.orekyuu.iroha.AggregateDataSource;
 import net.orekyuu.iroha.integration.Shard;
 import net.orekyuu.iroha.integration.scenario.IntegrationTestBase;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.seasar.doma.jdbc.dialect.Dialect;
 
@@ -14,39 +13,40 @@ public abstract class DomaIntegrationTest extends IntegrationTestBase {
 
   protected abstract Dialect getDialect();
 
+  UserDao userDao;
+
+  @BeforeEach
+  void setupDao() {
+    userDao = new UserDaoImpl(new DomaConfig(dataSource, getDialect()));
+  }
+
   @Test
   void insertToShard1() {
-    var dataSource = new AggregateDataSource(selector);
-    UserDao dao = new UserDaoImpl(new DomaConfig(dataSource, getDialect()));
-
     selector.use(
         () -> {
           var userEntity = new UserEntity("user1", 20);
-          var insertRows = dao.insert(userEntity);
-          Assertions.assertThat(userEntity.id).isNotNull();
-          Assertions.assertThat(insertRows).isEqualTo(1);
-          assertThat(table("users", Shard.SHARD_1)).hasNumberOfRows(1);
-          assertThat(table("users", Shard.SHARD_2)).hasNumberOfRows(0);
-          assertThat(table("users", Shard.SHARD_3)).hasNumberOfRows(0);
+          var insertRows = userDao.insert(userEntity);
+          assertThat(userEntity.id).isNotNull();
+          assertThat(insertRows).isEqualTo(1);
+          assertTable("users", Shard.SHARD_1).hasNumberOfRows(1);
+          assertTable("users", Shard.SHARD_2).hasNumberOfRows(0);
+          assertTable("users", Shard.SHARD_3).hasNumberOfRows(0);
         },
         Shard.SHARD_1);
   }
 
   @Test
   void insertToAll() {
-    var dataSource = new AggregateDataSource(selector);
-    UserDao dao = new UserDaoImpl(new DomaConfig(dataSource, getDialect()));
-
     selector.use(
         () -> {
           var userEntity = new UserEntity("user1", 20);
-          var insertRows = dao.insert(userEntity);
+          var insertRows = userDao.insert(userEntity);
 
-          Assertions.assertThat(userEntity.id).isNotNull();
-          Assertions.assertThat(insertRows).isEqualTo(3);
-          assertThat(table("users", Shard.SHARD_1)).hasNumberOfRows(1);
-          assertThat(table("users", Shard.SHARD_2)).hasNumberOfRows(1);
-          assertThat(table("users", Shard.SHARD_3)).hasNumberOfRows(1);
+          assertThat(userEntity.id).isNotNull();
+          assertThat(insertRows).isEqualTo(3);
+          assertTable("users", Shard.SHARD_1).hasNumberOfRows(1);
+          assertTable("users", Shard.SHARD_2).hasNumberOfRows(1);
+          assertTable("users", Shard.SHARD_3).hasNumberOfRows(1);
         },
         Shard.SHARD_1,
         Shard.SHARD_2,
@@ -55,22 +55,19 @@ public abstract class DomaIntegrationTest extends IntegrationTestBase {
 
   @Test
   void selectFromShard1() {
-    var dataSource = new AggregateDataSource(selector);
-    UserDao dao = new UserDaoImpl(new DomaConfig(dataSource, getDialect()));
-
-    selector.use(() -> dao.insert(new UserEntity("user1", 20)), Shard.SHARD_1);
-    selector.use(() -> dao.insert(new UserEntity("user2", 22)), Shard.SHARD_2);
-    selector.use(() -> dao.insert(new UserEntity("user3", 23)), Shard.SHARD_3);
+    selector.use(() -> userDao.insert(new UserEntity("user1", 20)), Shard.SHARD_1);
+    selector.use(() -> userDao.insert(new UserEntity("user2", 22)), Shard.SHARD_2);
+    selector.use(() -> userDao.insert(new UserEntity("user3", 23)), Shard.SHARD_3);
 
     selector.use(
         () -> {
-          Assertions.assertThat(dao.selectAll())
+          assertThat(userDao.selectAll())
               .hasSize(1)
               .first()
               .satisfies(
                   u -> {
-                    Assertions.assertThat(u.name).isEqualTo("user1");
-                    Assertions.assertThat(u.age).isEqualTo(20);
+                    assertThat(u.name).isEqualTo("user1");
+                    assertThat(u.age).isEqualTo(20);
                   });
         },
         Shard.SHARD_1);
@@ -78,16 +75,13 @@ public abstract class DomaIntegrationTest extends IntegrationTestBase {
 
   @Test
   void selectFromAllShards() {
-    var dataSource = new AggregateDataSource(selector);
-    UserDao dao = new UserDaoImpl(new DomaConfig(dataSource, getDialect()));
-
-    selector.use(() -> dao.insert(new UserEntity("user1", 20)), Shard.SHARD_1);
-    selector.use(() -> dao.insert(new UserEntity("user2", 22)), Shard.SHARD_2);
-    selector.use(() -> dao.insert(new UserEntity("user3", 23)), Shard.SHARD_3);
+    selector.use(() -> userDao.insert(new UserEntity("user1", 20)), Shard.SHARD_1);
+    selector.use(() -> userDao.insert(new UserEntity("user2", 22)), Shard.SHARD_2);
+    selector.use(() -> userDao.insert(new UserEntity("user3", 23)), Shard.SHARD_3);
 
     selector.use(
         () -> {
-          Assertions.assertThat(dao.selectAll())
+          assertThat(userDao.selectAll())
               .hasSize(3)
               .anyMatch(user -> user.name.equals("user1") && user.age == 20)
               .anyMatch(user -> user.name.equals("user2") && user.age == 22)
@@ -100,12 +94,9 @@ public abstract class DomaIntegrationTest extends IntegrationTestBase {
 
   @Test
   void selectFromAllShards_emptyResult() {
-    var dataSource = new AggregateDataSource(selector);
-    UserDao dao = new UserDaoImpl(new DomaConfig(dataSource, getDialect()));
-
     selector.use(
         () -> {
-          Assertions.assertThat(dao.selectAll()).isEmpty();
+          assertThat(userDao.selectAll()).isEmpty();
         },
         Shard.SHARD_1,
         Shard.SHARD_2,
@@ -114,15 +105,12 @@ public abstract class DomaIntegrationTest extends IntegrationTestBase {
 
   @Test
   void selectFromAllShards_existsEmptyResultSet() {
-    var dataSource = new AggregateDataSource(selector);
-    UserDao dao = new UserDaoImpl(new DomaConfig(dataSource, getDialect()));
-
-    selector.use(() -> dao.insert(new UserEntity("user1", 20)), Shard.SHARD_1);
-    selector.use(() -> dao.insert(new UserEntity("user3", 23)), Shard.SHARD_3);
+    selector.use(() -> userDao.insert(new UserEntity("user1", 20)), Shard.SHARD_1);
+    selector.use(() -> userDao.insert(new UserEntity("user3", 23)), Shard.SHARD_3);
 
     selector.use(
         () -> {
-          Assertions.assertThat(dao.selectAll())
+          assertThat(userDao.selectAll())
               .hasSize(2)
               .anyMatch(user -> user.name.equals("user1") && user.age == 20)
               .anyMatch(user -> user.name.equals("user3") && user.age == 23);
@@ -134,42 +122,38 @@ public abstract class DomaIntegrationTest extends IntegrationTestBase {
 
   @Test
   void batchInsert() {
-    var dataSource = new AggregateDataSource(selector);
-    UserDao dao = new UserDaoImpl(new DomaConfig(dataSource, getDialect()));
-
     selector.use(
         () -> {
           var user1 = new UserEntity("user1", 20);
           var user2 = new UserEntity("user2", 22);
-          var insertRows = dao.batchInsert(List.of(user1, user2));
-          Assertions.assertThat(user1.id).isNotNull();
-          Assertions.assertThat(user2.id).isNotNull();
-          Assertions.assertThat(user1.id).isNotEqualTo(user2.id);
-          Assertions.assertThat(insertRows).hasSize(2);
-          assertThat(table("users", Shard.SHARD_1)).hasNumberOfRows(2);
+          var insertRows = userDao.batchInsert(List.of(user1, user2));
+          assertThat(user1.id).isNotNull();
+          assertThat(user2.id).isNotNull();
+          assertThat(user1.id).isNotEqualTo(user2.id);
+          assertThat(insertRows).hasSize(2);
+          assertTable("users", Shard.SHARD_1).hasNumberOfRows(2);
+          assertTable("users", Shard.SHARD_2).hasNumberOfRows(0);
+          assertTable("users", Shard.SHARD_3).hasNumberOfRows(0);
         },
         Shard.SHARD_1);
   }
 
   @Test
   void batchInsert_allShard() {
-    var dataSource = new AggregateDataSource(selector);
-    UserDao dao = new UserDaoImpl(new DomaConfig(dataSource, getDialect()));
-
     selector.use(
         () -> {
           var user1 = new UserEntity("user1", 20);
           var user2 = new UserEntity("user2", 22);
-          var insertRows = dao.batchInsert(List.of(user1, user2));
-          Assertions.assertThat(user1.id).isNotNull();
-          Assertions.assertThat(user2.id).isNotNull();
-          Assertions.assertThat(user1.id).isNotEqualTo(user2.id);
-          Assertions.assertThat(insertRows).hasSize(2);
-          Assertions.assertThat(insertRows[0]).isEqualTo(3);
-          Assertions.assertThat(insertRows[1]).isEqualTo(3);
-          assertThat(table("users", Shard.SHARD_1)).hasNumberOfRows(2);
-          assertThat(table("users", Shard.SHARD_2)).hasNumberOfRows(2);
-          assertThat(table("users", Shard.SHARD_3)).hasNumberOfRows(2);
+          var insertRows = userDao.batchInsert(List.of(user1, user2));
+          assertThat(user1.id).isNotNull();
+          assertThat(user2.id).isNotNull();
+          assertThat(user1.id).isNotEqualTo(user2.id);
+          assertThat(insertRows).hasSize(2);
+          assertThat(insertRows[0]).isEqualTo(3);
+          assertThat(insertRows[1]).isEqualTo(3);
+          assertTable("users", Shard.SHARD_1).hasNumberOfRows(2);
+          assertTable("users", Shard.SHARD_2).hasNumberOfRows(2);
+          assertTable("users", Shard.SHARD_3).hasNumberOfRows(2);
         },
         Shard.SHARD_1,
         Shard.SHARD_2,
--- integration-test/src/main/java/net/orekyuu/iroha/integration/scenario/IntegrationTestBase.java
+++ integration-test/src/main/java/net/orekyuu/iroha/integration/scenario/IntegrationTestBase.java
@@ -1,9 +1,13 @@
 package net.orekyuu.iroha.integration.scenario;
 
+import static org.assertj.db.api.Assertions.assertThat;
+
 import java.util.EnumMap;
 import javax.sql.DataSource;
+import net.orekyuu.iroha.AggregateDataSource;
 import net.orekyuu.iroha.integration.EnumShardSelector;
 import net.orekyuu.iroha.integration.Shard;
+import org.assertj.db.api.TableAssert;
 import org.assertj.db.type.Table;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,9 +22,10 @@ public abstract class IntegrationTestBase {
 
   protected EnumShardSelector selector;
   private EnumMap<Shard, DataSource> shardMap;
+  protected AggregateDataSource dataSource;
 
-  protected final Table table(String name, Shard shard) {
-    return new Table(shardMap.get(shard), name);
+  protected final TableAssert assertTable(String name, Shard shard) {
+    return assertThat(new Table(shardMap.get(shard), name));
   }
 
   @BeforeEach
@@ -35,6 +40,8 @@ public abstract class IntegrationTestBase {
     shardMap.put(Shard.SHARD_2, shard2());
     shardMap.put(Shard.SHARD_3, shard3());
     selector = new EnumShardSelector(shardMap);
+
+    dataSource = new AggregateDataSource(selector);
   }
 
   @AfterEach
